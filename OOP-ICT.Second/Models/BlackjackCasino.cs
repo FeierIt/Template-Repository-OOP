955eaaad09f4072cf5987b52fffbf2cf6c398085
Andrey Puzenko <betzenko1996@gmail.com>
2024-03-14 21:19:43 +0300

    Second lab

---
+++ OOP-ICT.Second/Models/BlackjackCasino.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP_ICT.Second.Models;
+
+public class BlackjackCasino
+{
+    public void AwardWin(Player player, decimal amount)
+    {
+        player.Account.Deposit(amount);
+    }
+
+    public void DeductLoss(Player player, decimal amount)
+    {
+        player.Account.Withdraw(amount);
+    }
+
+    public void HandleBlackjack(Player player, decimal amount)
+    {
+        decimal winnings = amount * 1.5m;
+        AwardWin(player, winnings);
+    }
+}
